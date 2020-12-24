451d091ab3ccac93f9d05a02613e59dc338b20be
Ian Fleshman-Cooper <ianaac4@yahoo.com>
2020-12-24 02:16:04 -0800

    developed main inquirer prompt with  all switch cases

--- employeeTrackerCode.js
+++ employeeTrackerCode.js
@@ -11,12 +11,63 @@ const connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    runPrompt();
+    runMain();
 });
 
-function runPrompt() {
+function runMain() {
     inquirer
     .prompt({
-        
+      name: "main",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View all employees",
+        "View employees by department",
+        "View employees by manager",
+        "View employees by role",
+        "View budget by department",
+        "Add employee",
+        "Remove employee",
+        "Update employee role",
+        "Update employee manager"
+      ]
     })
+    .then(function(res) {
+        switch (res.main) {
+            case "View all employees":
+                employeeAll();
+                break;
+
+            case "View employees by department":
+                employeeDepartment();
+                break;
+
+            case "View employees by manager":
+                employeeManager();
+                break;
+
+            case "View employees by role":
+                employeeRole();
+                break;
+
+            case "View budget by department":
+                departmentBudget();
+                break;
+
+            case "Add employee":
+                addEmployee();
+                break;
+            case "Remove employee":
+                removeEmployee();
+                break;
+            
+            case "Update employee role":
+                updateRole();
+                break;
+            
+            case "Update employee manager":
+                updateManager();
+                break;        
+        }
+      });
 }
